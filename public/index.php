084ff1f8aa2348ffeda5be980b9489f09f2065fe
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-03-07 22:52:26 +0000

    Add ONGController

--- public/index.php
+++ public/index.php
@@ -1,11 +1,10 @@
 <?php
 require_once __DIR__.'/../vendor/autoload.php';
 
+use App\Controllers\ONGController;
 use App\core\Application;
 
 $app = new Application(dirname(__DIR__));
 
-$app->router->get('/', function(){
-    return 'Home Page';
-});
+$app->router->get('/ong',[ONGController::class, 'home']);
 $app->run();
---
+++ src/Controllers/ONGController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controllers;
+
+class ONGController
+{
+    public function home()
+    {
+
+    }
+
+    public function addONG()
+    {
+
+    }
+
+}
