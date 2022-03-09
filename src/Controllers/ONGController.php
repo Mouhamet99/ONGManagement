ea11a2b628735ddc8e83b7fa1ac7d2b88159c208
Mouhamet99 <dieyemouhamet1@gmail.com>
2022-03-09 16:49:57 +0000

    Add Database table and test it from a controller for a test

--- src/Controllers/ONGController.php
+++ src/Controllers/ONGController.php
@@ -3,11 +3,17 @@
 namespace App\src\Controllers;
 
 use App\core\Request;
+use App\src\Database\DBConnection;
 
 class ONGController extends Controller
 {
     public function home(): string
     {
+        $db = new DBConnection('localhost', 'ong_nous_les_femmes','root', '');
+        $req = $db->getPDO()->query("SELECT * FROM region");
+        $regions = $req->fetchAll();
+//        var_dump($region);
+        return json_encode($regions);
         return $this->render('home');
     }
 
@@ -19,5 +25,7 @@ class ONGController extends Controller
 
         return $this->render('newOng');
     }
+
+
 
 }
